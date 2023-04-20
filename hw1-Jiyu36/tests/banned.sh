3e21d928aed5c82e5057de7ab9ba415cef5f5608
Jiyu36 <123434428+Jiyu36@users.noreply.github.com>
2023-04-19 18:43:02 -0700

    Delete hw1-Jiyu36 directory

--- hw1-Jiyu36/tests/banned.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-badfile=$1
-badwords=$(paste -s -d '|' $badfile)
-
-echo searching for banned keywords!
-grep -H -n '.*' src/* | sed 's:import.*$::g' | sed 's:--.*$::g' | grep --color -E '[[:<:]]('$badwords')[[:>:]]'
-
